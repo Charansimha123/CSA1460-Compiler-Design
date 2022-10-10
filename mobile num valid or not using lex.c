8c7e7b6e4318cd0204cea4cd92951a43a39bbc80
Naven2426 <112463079+Naven2426@users.noreply.github.com>
2022-10-10 13:12:07 +0530

    Update and rename mobile num valid or not using lex.cpp to mobile num valid or not using lex.c

--- mobile num valid or not using lex.c
+++ mobile num valid or not using lex.c
@@ -1,19 +1,10 @@
-/* Lex Program to check valid Mobile Number */
-
 %{
-	/* Definition section */
+
 %}
-
-/* Rule Section */
 %%
-
 [1-9][0-9]{9} {printf("\nMobile Number Valid\n");}
-
 .+ {printf("\nMobile Number Invalid\n");}
-
 %%
-
-// driver code
 int main()
 {
 	printf("\nEnter Mobile Number : ");
