e657f93a6606b59ffe66550b25d526d915a6a4b8
Naven2426 <112463079+Naven2426@users.noreply.github.com>
2022-10-10 08:52:33 +0530

    Update first for predictive parser.cpp

--- first for predictive parser.cpp
+++ first for predictive parser.cpp
@@ -8,44 +8,30 @@ int i;
 char choice; char c;
 char result[20];
 printf("How many number of productions ? :"); scanf(" %d",&numOfProductions);
-for(i=0;i<numOfProductions;i++)//read production string eg: E=E+T
+for(i=0;i<numOfProductions;i++)
 {
 printf("Enter productions Number %d : ",i+1); scanf(" %s",productionSet[i]);
 }
 do
 {
 printf("\n Find the FIRST of :"); scanf(" %c",&c);
-FIRST(result,c); //Compute FIRST; Get Answer in 'result' array printf("\n FIRST(%c)= { ",c);
+FIRST(result,c); 
 for(i=0;result[i]!='\0';i++)
-printf(" %c ",result[i]);	//Display result printf("}\n");
+printf(" %c ",result[i]);	
 
 printf("press 'y' to continue : "); scanf(" %c",&choice);
 }
 while(choice=='y'||choice =='Y');
 }
-/*
-*Function FIRST:
-*Compute the elements in FIRST(c) and write them
-*in Result Array.
-*/
 void FIRST(char* Result,char c)
 {
 int i,j,k;
 char subResult[20]; int foundEpsilon; subResult[0]='\0'; Result[0]='\0';
-//If X is terminal, FIRST(X) = {X}. if(!(isupper(c)))
 {
 addToResultSet(Result,c); return ;
 }
-//If X is non terminal
-//Read each production for(i=0;i<numOfProductions;i++)
 {
-//Find production with X as LHS if(productionSet[i][0]==c)
 {
-//If X ? ? is a production, then add ? to FIRST(X). if(productionSet[i][2]=='$') addToResultSet(Result,'$');
-//If X is a non-terminal, and X ? Y1 Y2 … Yk
-//is a production, then add a to FIRST(X)
-//if for some i, a is in FIRST(Yi),
-//and ? is in all of FIRST(Y1), …, FIRST(Yi-1). else
 {
 j=2;
 while(productionSet[i][j]!='\0')
@@ -57,7 +43,6 @@ if(subResult[k]=='$')
 
 foundEpsilon=1; break;
 }
-//No ? found, no need to check next element if(!foundEpsilon)
 break; j++;
 }
 }
@@ -65,10 +50,6 @@ break; j++;
 }
 return ;
 }
-/* addToResultSet adds the computed
-*element to result set.
-*This code avoids multiple inclusion of elements
-*/
 void addToResultSet(char Result[],char val)
 {
 int k;
